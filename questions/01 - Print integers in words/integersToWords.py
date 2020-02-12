ef03d4a73d9c0cfe1631da1d1f8ab0804a6430a9
hm-y <hmy0hmy@gmail.com>
2020-02-11 23:48:04 -0500

    Update Q1

--- questions/01 - Print integers in words/integersToWords.py
+++ questions/01 - Print integers in words/integersToWords.py
@@ -2,7 +2,14 @@
 # Given an integer between 1-999, print it in words
 # Ex: 419 => Four Hundred Nineteen
 
+# Question 2:
+# Widen the range up to 999999
+# Ex: 419 502 => Four Hundred Nineteen Thousand Five Hundred Two
+
 def intToWord(num):
+    thousands = num // 1000
+    num %= 1000
+
     # Dictionaries
     singles = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
     tens = ["Twenty", "Thirty", "Forty","Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
@@ -26,6 +33,9 @@ def intToWord(num):
     if oneDigit > 0 and tenDigit != 1:
         word += singles[oneDigit-1]
 
+    if thousands > 0:
+        word = intToWord(thousands) + " Thousand " + word
+
     return word
 
 # Test Cases
@@ -38,24 +48,9 @@ print("810: " + intToWord(810))
 print("17: " + intToWord(17))
 print("100: " + intToWord(100))
 
-# Question 2:
-# Widen the range up to 999999
-# Ex: 419 502 => Four Hundred Nineteen Thousand Five Hundred Two
-
-def intToWordUpToMillion(num):
-    word = ""
-
-    thousands = num // 1000
-    if thousands > 0:
-        word += intToWord(thousands) + " Thousand "
-
-    word += intToWord(num%1000)
-    return word
-
-
-# Test Cases
-print("1000: " + intToWordUpToMillion(1000))
-print("999999: " + intToWordUpToMillion(999999))
-print("100000: " + intToWordUpToMillion(100000))
-print("123456: " + intToWordUpToMillion(123456))
-print("505050: " + intToWordUpToMillion(505050))
+# Test Cases 2
+print("1000: " + intToWord(1000))
+print("999999: " + intToWord(999999))
+print("100000: " + intToWord(100000))
+print("123456: " + intToWord(123456))
+print("505050: " + intToWord(505050))
